0799ccfcbc667f9c75b3c67add8de2f90b6b5a60
Gustavo Mockaitis - GBMA <168574383+gusmock@users.noreply.github.com>
2025-12-08 15:33:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -633,59 +633,46 @@ if run_btn and not validation_errors:
 st.markdown("---")
 
 footer_html = """
-<style>
-.footer-container {
-    width: 100%;
-    text-align: center;
-    font-family: sans-serif;
-    color: #444;
-    margin-top: 30px;
-}
+<div style="width:100%; text-align:center; font-family:sans-serif; color:#444; margin-top:30px;">
+    <h4 style="margin-bottom:5px;">Desenvolvido por: Prof. Dr. Gustavo Mockaitis</h4>
+    <p style="margin-top:0; font-size:0.9em;">GBMA / FEAGRI / UNICAMP</p>
 
-.footer-links {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    gap: 10px;
-    margin-top: 15px;
-}
-
-.footer-links img {
-    height: 32px;
-}
-</style>
-
-<div class="footer-container">
-    <h4 style="margin-bottom: 5px;">Desenvolvido por: Prof. Dr. Gustavo Mockaitis</h4>
-    <p style="margin-top: 0; font-size: 0.9em;">GBMA / FEAGRI / UNICAMP</p>
-
-    <div class="footer-links">
+    <div style="display:flex; flex-wrap:wrap; justify-content:center; gap:10px; margin-top:15px;">
         <a href="https://arxiv.org/abs/2507.05960" target="_blank">
-            <img src="https://img.shields.io/badge/arXiv-Preprint-B31B1B" />
+            <img src="https://img.shields.io/badge/arXiv-Preprint-B31B1B"
+                 style="height:32px;"/>
         </a>
         <a href="https://github.com/gusmock/mono_polyauxic_kinetics/" target="_blank">
-            <img src="https://img.shields.io/badge/GitHub-Repo-181717" />
+            <img src="https://img.shields.io/badge/GitHub-Repo-181717"
+                 style="height:32px;"/>
         </a>
         <a href="https://orcid.org/0000-0002-4231-1056" target="_blank">
-            <img src="https://img.shields.io/badge/ORCID-iD-A6CE39" />
+            <img src="https://img.shields.io/badge/ORCID-iD-A6CE39"
+                 style="height:32px;"/>
         </a>
         <a href="https://scholar.google.com/citations?user=yR3UvuoAAAAJ&hl=pt-BR&oi=ao" target="_blank">
-            <img src="https://img.shields.io/badge/Google%20Scholar-Perfil-4285F4" />
+            <img src="https://img.shields.io/badge/Google%20Scholar-Perfil-4285F4"
+                 style="height:32px;"/>
         </a>
         <a href="https://www.researchgate.net/profile/Gustavo-Mockaitis" target="_blank">
-            <img src="https://img.shields.io/badge/ResearchGate-Perfil-00CCBB" />
+            <img src="https://img.shields.io/badge/ResearchGate-Perfil-00CCBB"
+                 style="height:32px;"/>
         </a>
         <a href="http://lattes.cnpq.br/1400402042483439" target="_blank">
-            <img src="https://img.shields.io/badge/Lattes-CV-003399" />
+            <img src="https://img.shields.io/badge/Lattes-CV-003399"
+                 style="height:32px;"/>
         </a>
         <a href="https://www.linkedin.com/in/gustavo-mockaitis/" target="_blank">
-            <img src="https://img.shields.io/badge/LinkedIn-Conectar-0A66C2" />
+            <img src="https://img.shields.io/badge/LinkedIn-Conectar-0A66C2"
+                 style="height:32px;"/>
         </a>
         <a href="https://www.webofscience.com/wos/author/record/J-7107-2019" target="_blank">
-            <img src="https://img.shields.io/badge/Web%20of%20Science-Perfil-5E33BF" />
+            <img src="https://img.shields.io/badge/Web%20of%20Science-Perfil-5E33BF"
+                 style="height:32px;"/>
         </a>
         <a href="http://feagri.unicamp.br/mockaitis" target="_blank">
-            <img src="https://img.shields.io/badge/UNICAMP-Institucional-CC0000" />
+            <img src="https://img.shields.io/badge/UNICAMP-Institucional-CC0000"
+                 style="height:32px;"/>
         </a>
     </div>
 </div>
